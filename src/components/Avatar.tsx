0d642a7255db775a8331c10aac9f1eeaea0caf22
JeanJoseph128 <josephjjean128@gmail.com>
2024-06-05 19:21:40 -0300

    fix2

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 interface AvatarProps {
     imgSource: string;
     username: string;
@@ -5,14 +7,23 @@ interface AvatarProps {
 
 const Avatar = (props: AvatarProps) => {
   return (
-    <div className="p-6 block text-2xl">
+    <motion.div
+    animate={{ opacity: 1 }}
+    initial={{
+      opacity: 0,
+    }}
+    className="flex flex-col justify-center items-center"
+  >
+    <div className="flex flex-col justify-center items-center gap-2">
       <img
+        className="border-2 border-neutral rounded-full w-28"
         src={props.imgSource}
-        alt="profile img"
-        className="rounded-full m-auto border border-zinc-500 w-28"
       />
-      <h1 className="text-center mt-2">{props.username}</h1>
+      <div className="flex flex-row items-center gap-2">
+        <h1 className="text-xl">{props.username}</h1>
+      </div>
     </div>
+  </motion.div>
   );
 };
 
